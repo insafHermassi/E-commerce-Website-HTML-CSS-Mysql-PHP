25947dffae0c3255ba0a46baab99a6add3b4d75c
insafHermassi <insafhermassi27@gmail.com>
2023-03-12 19:54:49 +0100

    add session

--- connexion.php
+++ connexion.php
@@ -7,6 +7,17 @@ $user = true;
 if (!empty($_POST))// button sauvegarder clicked
 { 
 $user =  ConnectVisiteur($_POST);
+if (count($user) > 0){ // utilisateur connecté
+   
+  session_start();
+  $_SESSION['email'] = $user['email'];
+  $_SESSION['nom'] = $user['nom'];
+  $_SESSION['prenom'] = $user['prenom'];
+  $_SESSION['mp'] = $user['mp'];
+  $_SESSION['telephone'] = $user['telephone'];
+  
+  header ('location: profile.php'); //redirection vers la page profile
+}
 
 }
 
@@ -19,7 +30,7 @@ $user =  ConnectVisiteur($_POST);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-SHOP</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.16.8/sweetalert2.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.16.6/sweetalert2.min.css">
   </head>
 <body>
    <?php
@@ -39,7 +50,7 @@ $user =  ConnectVisiteur($_POST);
             <input type="password" name = "mp" class="form-control" id="exampleInputPassword1" placeholder="Password">
           </div>
 
-        <button type="submit" class="btn btn-primary">Sauvegarder</button>
+        <button type="submit" class="btn btn-primary">Connecter</button>
 
       </form>
 
@@ -51,7 +62,7 @@ $user =  ConnectVisiteur($_POST);
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script> src= "https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.16.8/sweetalert2.all.min.js"</script>
+<script> src= "https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.16.6/sweetalert2.min.js"</script>
  <?php
  if ( !$user){
  print "
